7af24eea10138769d93984da9569be98156c0759
Tony <158102715+tonyGERR@users.noreply.github.com>
2024-07-09 10:36:28 -0400

    added web interface comment to query shortcut

--- SIPServer-Installer/InstallUtils/SvcQuery.bat
+++ SIPServer-Installer/InstallUtils/SvcQuery.bat
@@ -1,4 +1,10 @@
 @echo off
 sc query SIPServer
+echo ========================================================
+echo Note: 
+echo you can also optionally use the readonly Web interface:
+echo 		http://localhost:8080/portal/index.html 
+echo 		and login as uid:freeswitch  pwd:works
+echo ========================================================
 Pause
 exit
