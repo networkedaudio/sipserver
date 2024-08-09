1392870bda6f51a75630a612cd66bb92f706fab8
Tony <158102715+tonyGERR@users.noreply.github.com>
2024-08-09 14:52:30 -0400

    Updated default directory as per Corey's email

--- SIPServer-Installer/InstallUtils/SetIP.bat
+++ SIPServer-Installer/InstallUtils/SetIP.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Setting IP addresses... %1 %2
-%3\grep.exe %3\conf\vars.xml  10.8.91.10 %1
-%3\grep.exe %3\conf\vars.xml  10.8.90.10 %2
+%3\grep.exe %3\conf\vars.xml  10.8.91.10 %1  2>NUL >NUL
+%3\grep.exe %3\conf\vars.xml  10.8.90.10 %2  2>NUL >NUL
 del %3\grep.exe
 echo Done.
